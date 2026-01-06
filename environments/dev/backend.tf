3934b386618de4abc4ed154be2ee5998129f7a7b
volv09 <253123713+volv09@users.noreply.github.com>
2026-01-06 19:33:06 +0100

    Modified bucket name because the initial bucket name is already taken in S3

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3-terraform-backend-states"
+    bucket = "s3-terraform-backend-state-files"
     key    = "environments/dev/terraform.tfstate"
     region = "eu-west-1"
   }
