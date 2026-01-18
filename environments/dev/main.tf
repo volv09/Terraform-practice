4a4f5863d73523297729534799afaa6c8bf79513
volv09 <253123713+volv09@users.noreply.github.com>
2026-01-18 15:26:42 +0100

    Added new rule for ingress_rules

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -37,7 +37,8 @@ module "sg_rules" {
 
   ingress_rules = [
     { from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
-    { from_port = 22, to_port = 22, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
+    { from_port = 22, to_port = 22, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
+    { from_port  = 443, to_port  = 443, protocol  = "tcp", cidr_blocks = ["0.0.0.0/0"] }
   ]
 
   egress_rules = [
